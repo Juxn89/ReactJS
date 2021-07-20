c2768219ed9166f73be94087905402b24dac0568
jgomez_89 <juan.gomez@axxis-systems.com>
2021-07-19 22:18:14 -0600

    Instalación de Enzyme, se agrega referencia de wojtekmaj/enzyme-adapter-react-17 y se realiza pruebas sobre el renderizado del compomente PrimeraApp.

--- 03-counter-app/src/test/PrimeraApp.test.js
+++ 03-counter-app/src/test/PrimeraApp.test.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+// import {render} from '@testing-library/react';
 import PrimeraApp from '../PrimeraApp';
+import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
 
 describe('Test on <PrimeraApp/>', () => {
-    test('', () => {
-        const saludo = 'Hola, soy Goku';
-        const {getByText} = render(<PrimeraApp saludo={saludo}/>);
+    /* Se comenta porque ahora se empleará Enzyne */
+    // test('', () => {
+    //     const saludo = 'Hola, soy Goku!';
+    //     const {getByText} = render(<PrimeraApp saludo={saludo}/>);
+    //     expect(getByText(saludo)).toBeInTheDocument();
+    // });
 
-        expect(getByText(saludo)).toBeInTheDocument();
+    test('Test on <PrimeraApp/>', () => {
+        const saludo = 'Hola, soy Goku!';
+        const wrapper = shallow(<PrimeraApp saludo={saludo}/>);
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Should show subtitle sent by props', () => {
+        const saludo = 'Hola, soy Goku';
+        const subtitulo = 'subtitulo desde test!';        
+
+        const wrapper = shallow(<PrimeraApp saludo={saludo} subtitulo={subtitulo}/>);
+        const textoParrafo = wrapper.find('h3').text();
+        console.log(textoParrafo);
+
+        expect(textoParrafo).toBe(subtitulo);
     });
 });
