5950b9cb25c448e190d12c8be531c52e2e380f0a
jgomez_89 <juan.gomez@axxis-systems.com>
2021-08-10 20:23:49 -0600

    Ejemplo del uso de custom hook useForm para el manejo de formulario

---
+++ 05-hook-app/src/components/hooks/useForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useState } from "react";
+
+const useForm = (initialState = {}) => {
+    const [values, setValues] =  useState(initialState);
+
+    const handleInputChange = ({target}) => {
+        setValues({
+            ...values,
+            [target.name]: target.value
+        });
+    }
+
+    return [values, handleInputChange];
+}
+
+export default useForm;
