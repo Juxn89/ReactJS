29caeace2f6398727e74e9ef99c44012d1994793
jgomez_89 <juan.gomez@axxis-systems.com>
2021-08-11 21:49:28 -0600

    Ejemplo de custom hook useFetch y hook useRef

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FocusScreen from './components/04-useRef/FocusScreen';
+// import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
 // import SimpleForm from './components/02-useEffect/SimpleForm';
 // import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
 // import CounterApp from './components/01-useState/CounterApp';
-import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
+// import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
 
 ReactDOM.render(
-  <FormWithCustomHook />,
+  <FocusScreen />,
   document.getElementById('root')
 );
