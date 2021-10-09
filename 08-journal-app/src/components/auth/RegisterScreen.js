70c615ae3eaa5d5d63b9fd51222b41a740d51dd9
Juan Gómez <juan.gomez@axxis-systems.com>
2021-10-09 15:22:56 -0600

    Se agregan estilos a LoginScreen y RegisterScreen

--- 08-journal-app/src/components/auth/RegisterScreen.js
+++ 08-journal-app/src/components/auth/RegisterScreen.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const RegisterScreen = () => {
     return(
         <>
-            <div>
-                <h1>RegisterScreen</h1>
-            </div>
+           <h3 className="auth__title">Login</h3>
+           <form>
+               <input className="auth__input" type="text" placeholder="Name" name="name" autoComplete="off"/> 
+               <input className="auth__input" type="text" placeholder="Email" name="email" autoComplete="off"/>
+               <input className="auth__input" type="password" placeholder="Password" name="password"/>
+               <input className="auth__input" type="password" placeholder="Confirm password" name="password2"/>
+               <button className="btn btn-primary btn-block mb-5" type="submit">Register</button>
+
+               <Link className="link" to="/auth/login">Already register</Link>
+           </form>
         </>
     );
 };
