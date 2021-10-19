62708be8661c047a47ab7f2d3e030d483d2ec418
Juan Gómez <juan.gomez@axxis-systems.com>
2021-10-18 20:57:47 -0600

    Se mantiene el estado de la autenticación al recargar.

--- 08-journal-app/src/routers/AppRouter.js
+++ 08-journal-app/src/routers/AppRouter.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import JournalScreen from "../components/journal/JournalScreen";
 import AuthRouter from "./AuthRouter";
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { useDispatch } from "react-redux";
+import { login } from "../actions/auth";
 
 const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect( () => {
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if(user?.uid)
+                dispatch(login(user.uid, user.displayName));
+        });
+    }, []);
+
     return(
         <Router>
             <div>
