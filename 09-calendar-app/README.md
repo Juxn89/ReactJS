b042f02ca91a80eae92c130d6c4c3305060ce2b7
juan.gomez <gomezj01@stepstone.com>
2022-01-10 11:26:18 -0600

    Init project Calendar App

---
+++ 09-calendar-app/README.md
@@ -0,0 +1,5 @@
+# Calendar App
+## Section 23 - MERN Calendar - Struct and Design
+
+This app use the following npm **modules**
+- `react-router-dom`
---
+++ 09-calendar-app/src/router/AppRouter.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { LoginScreen } from "../components/auth/LoginScreen";
+import { CalendarScreen } from "../components/calendar/CalendarScreen";
+
+export const AppRouter = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path="/login" element={ <LoginScreen/> }/>
+                    <Route exact path="/" element={ <CalendarScreen/> }/>
+                    <Route path='*' element={ <Navigate replace to='/' /> }/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
