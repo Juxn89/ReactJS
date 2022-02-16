d1f0274a68b6e6847333ef533f0ac943d3e4f235
juan.gomez <gomezj01@stepstone.com>
2022-02-16 16:31:57 -0600

    Added ReactBigCalenda and Moment npm package, config calendar on CalanedarScreen component

--- 09-calendar-app/src/components/calendar/CalendarScreen.js
+++ 09-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,9 +1,42 @@
 import React from "react";
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import { Navbar } from "../ui/Navbar";
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { messages } from "../../helpers/calendar-messages-es";
+
+import 'moment/locale/es';
+moment.locale('es');
+
+const localizer = momentLocalizer(moment);
+const events = [{title: 'titulo', start: moment().toDate(), end: moment().add(2,'hours').toDate(), bgcolor:'#fafafa'}];
 
 export const CalendarScreen = () => {
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        };
+
+        return {
+            style
+        }
+    };
+
     return (
-        <>
-            <h1>Calendar Screen</h1>
-        </>
+        <div className="calendar-screen">
+            <Navbar/>
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                messages={messages}
+                eventPropGetter={eventStyleGetter}
+                />
+        </div>
     );
 }
