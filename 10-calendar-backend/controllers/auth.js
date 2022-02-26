28501b6a91796376efaebc3dc9151deed894dfbe
juan.gomez <gomezj01@stepstone.com>
2022-02-26 09:31:38 -0600

    Created custom middleware validateFields.

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -6,16 +6,6 @@ const { validationResult } = require('express-validator');
 const crearUsuario = (req, res = response) => {
     const { name, email, password } = req.body;
 
-    // handle errors
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
-
     res.status(201).json({
         ok: true,
         msg: 'registro',
@@ -27,16 +17,6 @@ const crearUsuario = (req, res = response) => {
 
 const loginUsuario = (req, res = response) => {
     const { email, password } = req.body;
-
-    // handle errors
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
 
     res.status(201).json({
         ok: true,
--- 10-calendar-backend/routes/auth.js
+++ 10-calendar-backend/routes/auth.js
@@ -1,20 +1,23 @@
 const {Router} = require('express');
 const { check } = require('express-validator');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+const { validateFields } = require('../middlewares/validateFields');
 const router = Router();
 
 router.post('/new', 
             [
                 check('name', 'El nombre es obligatorio').not().isEmpty(),
                 check('email', 'El email es obligatorio').isEmail(),
-                check('password', 'El password debe de ser de 6 carácteres').isLength({min:6})
+                check('password', 'El password debe de ser de 6 carácteres').isLength({min:6}),
+                validateFields
             ], 
             crearUsuario);
 
 router.post('/', 
             [
                 check('email', 'El email es obligatorio').isEmail(),
-                check('password', 'El password es obligatorio').not().isEmpty()
+                check('password', 'El password es obligatorio').not().isEmpty(),
+                validateFields
             ],
             loginUsuario);
 
